b9f23655b67cca61901d92b3641b0ad29fa8137d
Anand Nandakumar <anandnk90@gmail.com>
2020-06-20 19:03:40 -0400

    updates to requirements and docker files

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,17 +1,18 @@
 #to run: ./upload_docker.sh <<dockerpath>> <<version number>> <<docker image>>
 
-# Step 1:
+# Step 1:miniku
 # Create dockerpath
 #dockerpath=anandnk90/udacitycapstone
 dockerpath=${1}
 version=${2}
+imageid=${3}
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 echo "Logging in using password file"
 cat dockerpasswd.txt | docker login --username anandnk90 --password-stdin
-docker tag ${3} $dockerpath:$version
+docker tag $imageid $dockerpath:$version
 
 # Step 3:
 # Push image to a docker repository
