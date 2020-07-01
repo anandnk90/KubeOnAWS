88509b04837289c83e4e1c79ad15bcace380f9f8
Anand Nandakumar <anandnk90@gmail.com>
2020-07-01 12:49:13 -0400

    Create script files for steps

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+cluster_name=capstone-cluster
+cluster_data=$(eksctl get cluster | grep capstone-cluster)
+if [[ -z cluster_data ]]; then
+  eksctl create cluster --name cluster1 --version 1.16 --region us-west-2 
+fi
 
-eksctl create cluster --name cluster1 --version 1.16 --region us-west-2 
---
+++ deploy-app-on-k8s.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cluster_name=capstone-cluster
+cluster_data=$(eksctl get cluster | grep capstone-cluster)
+if [[ -z cluster_data ]]; then
+  echo "Cluster not created!"
+  exit 1;
+fi
+aws eks --region us-west-2 update-kubeconfig --name $cluster_name
+kubectl config set-context $cluster-name
+kubectl apply -f kubernetesdeployment.yml
+
