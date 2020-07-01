3268768b2effe4d1d169109d258f38b76b630e77
Anand Nandakumar <anandnk90@gmail.com>
2020-07-01 13:01:47 -0400

    Fix issues

--- Jenkinsfile
+++ Jenkinsfile
@@ -10,7 +10,7 @@ pipeline {
          }
          stage('Set up infra') {
 		steps {
-	       sh 'sh ./create-cluster.sh'
+	       sh './create-cluster.sh'
              }
         }
          }
--- create-cluster.sh
+++ create-cluster.sh
@@ -2,7 +2,11 @@
 
 cluster_name=capstone-cluster
 cluster_data=$(eksctl get cluster | grep capstone-cluster)
-if [[ -z cluster_data ]]; then
-  eksctl create cluster --name cluster1 --version 1.16 --region us-west-2 
+echo $cluster_data
+if [[ -z $cluster_data ]]; then
+  #eksctl create cluster --name cluster1 --version 1.16 --region us-west-2 
+  echo "Cluster creation complete ${cluster_name}"
+else
+  echo "Cluster already exists"
 fi
 
