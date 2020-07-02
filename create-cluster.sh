1933b0f8e6e27179f4aa66cf9631ea313c931b98
Anand Nandakumar <anandnk90@gmail.com>
2020-07-01 21:33:21 -0400

    add profile to eksctl

--- create-cluster.sh
+++ create-cluster.sh
@@ -4,7 +4,7 @@ cluster_name=capstone-cluster
 cluster_data=$(eksctl get cluster | grep capstone-cluster)
 echo $cluster_data
 if [[ -z $cluster_data ]]; then
-  #eksctl create cluster --name cluster1 --version 1.16 --region us-west-2 
+  eksctl --profile jenkins create cluster --name cluster1 --version 1.16 --region us-west-2 
   echo "Cluster creation complete ${cluster_name}"
 else
   echo "Cluster already exists"
