75336015e055b0c59f266651d832c13471fa8211
Anand Nandakumar <anandnk90@gmail.com>
2020-07-02 22:07:28 -0400

    push to docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,14 +1,6 @@
 # Step 1:
 # Set variables
-dockerpath=anandnk90/udacitycapstone
-imageid=docker images -q udacitycapstone/app
-version=v1
-
-# Step 2:  
-# Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
 cat $DOCKERPASSWD | docker login --username anandnk90 --password-stdin
-docker tag $imageid $dockerpath:$version
 
 # Step 3:
 # Push image to a docker repository
