ffdb53ad3a3651fef0dab88fcabc7063f4af2195
Anand Nandakumar <anandnk90@gmail.com>
2020-07-02 22:21:05 -0400

    Fix repo  name

--- upload_docker.sh
+++ upload_docker.sh
@@ -4,4 +4,4 @@ echo $DOCKERPASSWD | docker login --username anandnk90 --password-stdin
 
 # Step 3:
 # Push image to a docker repository
-docker push udacitycapstone/app:latest
+docker push anandnk90/udacitycapstone:latest
