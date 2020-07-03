7adc75ddd74dbd363ecf22f5d77af41b0d0daadc
Anand Nandakumar <anandnk90@gmail.com>
2020-07-02 22:14:42 -0400

    Fix upload script

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,7 +1,7 @@
 # Step 1:
 # Set variables
-cat $DOCKERPASSWD | docker login --username anandnk90 --password-stdin
+echo $DOCKERPASSWD | docker login --username anandnk90 --password-stdin
 
 # Step 3:
 # Push image to a docker repository
-docker push $dockerpath
+docker push udacitycapstone/app:latest
